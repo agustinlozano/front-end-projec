3a46f5ee2708814e0de75dc9c78d62b45a561bb4
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2021-07-29 20:43:59 -0300

    Hace cambios en test tarea-1 Se hacen pequenios cambios a partir de leer la documentacion y algunos ejemplos.

--- cypress/integration/clase-6/tarea1-test.js
+++ cypress/integration/clase-6/tarea1-test.js
@@ -10,37 +10,43 @@ context('Tarea uno - Clase 6', () => {
     });
 
     describe('pruebas para el event handler, cargar interantes', () => {
-        it('testea el boton cargar', () => {    
+        it('testea el boton cargar', () => {
             cy.get('#cargar-integrantes').should('have.attr', 'type', 'submit');
         });
 
         it('se asegura que no haya integrantes antes de cargar los nuevos', () => {
-            cy.get('#integrantes').children().should('have.length', 0);
+            cy.get('#integrantes')
+                .children()
+                .should('have.length', 0);
         });
     });
 
     describe('prueba la funcionalidad de de la aplicacion', () => {
         const cantidadIntegrantes = generarNumeroEntre(3, 8);
-        const edadesInegrantes = [];
 
-        for (let i = 0; i<cantidadIntegrantes; i++) {
-            edadesInegrantes[i] = generarNumeroEntre(1, 99);
-        }
-        
-        const resultados = calcularRultados(edadesInegrantes);
-        const keys = Object.keys(resultados);
-        
         it('se asegura que los integrantes se generen correctamente', () => {
             const VALOR_EJEMPLO = cantidadIntegrantes;
-            
-            cy.get('#numero-integrantes').type(VALOR_EJEMPLO);
-            
+
+            cy.get('#numero-integrantes')
+                .type(VALOR_EJEMPLO)
+                .should('have.value', VALOR_EJEMPLO);
+
             cy.get('#cargar-integrantes').click();
 
-            cy.get('#integrantes').children().should('have.length', VALOR_EJEMPLO);
+            cy.get('#integrantes')
+                .children()
+                .should('have.length', VALOR_EJEMPLO);
         });
 
         it('se asegura que los calculos sean correctos', () => {
+            const edadesInegrantes = [];
+            for (let i = 0; i < cantidadIntegrantes; i++) {
+                edadesInegrantes[i] = generarNumeroEntre(1, 99);
+            }
+
+            const resultados = calcularRultados(edadesInegrantes);
+            const keys = Object.keys(resultados);
+            
             cy.get('.edad-integrantes').each(($integrante, i) => {
                 $integrante[0].value = edadesInegrantes[i];
             });
@@ -57,7 +63,7 @@ context('Tarea uno - Clase 6', () => {
 });
 
 function generarNumeroEntre(minimo, maximo) {
-    const numero = Math.floor(Math.random() * ((maximo + 1) - minimo ) + minimo);
+    const numero = Math.floor(Math.random() * ((maximo + 1) - minimo) + minimo);
     return numero;
 }
 
@@ -71,7 +77,7 @@ function calcularRultados(edadesInegrantes) {
     resultados['promedio'] = promedioEdad;
     resultados['mayor'] = mayorEdad;
     resultados['menor'] = menorEdad;
-    
+
     return resultados;
 }
 
@@ -114,6 +120,6 @@ function calcularPromedio(array) {
     array.forEach(elemento => {
         suma = suma + elemento;
     });
-    
+
     return suma / cantidadIntegrantes;
 }
